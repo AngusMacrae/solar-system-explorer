fca44cce376f7c506eb3e6d55d5f311d6341ee43
Angus Macrae <angusdmacrae@gmail.com>
2020-06-13 17:55:08 +0800

    Begin work on planet view

--- client/src/views/PlanetView.vue
+++ client/src/views/PlanetView.vue
@@ -1,5 +1,27 @@
 <template>
   <div>
-    <h1>{{$route.params.planet}}</h1>
+    <h1>{{$route.params.body}}</h1>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      // body: fetch().then(response => response.json())
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../scss/variables";
+
+// div {
+//   height: 100vh;
+//   background-image: url("../assets/jupiter-wide.jpg");
+//   background-position: center;
+//   background-size: cover;
+//   background-repeat: no-repeat;
+// }
+</style>
