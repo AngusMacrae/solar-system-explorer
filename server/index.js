4a9400554563626a2f07b08715823323c47d719d
Angus Macrae <angusdmacrae@gmail.com>
2020-06-14 20:53:52 +0800

    Add moon API routes

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const app = express();
 const MongoClient = require('mongodb').MongoClient;
 
 const planets = require('./routes/api/planets.js');
-// const moons = require('./routes/api/moons.js');
+const moons = require('./routes/api/moons.js');
 // const bodies = require('./routes/api/bodies.js');
 
 const connectionString = 'mongodb+srv://client:client@cluster0-kc2zl.mongodb.net/<dbname>?retryWrites=true&w=majority';
@@ -14,13 +14,8 @@ MongoClient.connect(connectionString, { useUnifiedTopology: true })
     console.log('Connected to database');
     app.locals.db = client.db('solarSystemExplorer');
     app.use(cors());
-    // const planetsCollection = db.collection('planets');
-    // app.get('/api/planets/:planetName', async (req, res) => {
-    //   const planet = await planetsCollection.findOne({ name: req.params.planetName });
-    //   res.json(planet);
-    // });
     app.use('/api/planets', planets);
-    // app.use('/api/moons', moons);
+    app.use('/api/moons', moons);
     // app.use('/api/bodies', bodies);
   })
   .catch(error => console.error(error));
--- server/routes/api/moons.js
+++ server/routes/api/moons.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/:bodyName', async (req, res) => {
-  res.send(`Data about ${req.params.bodyName}`);
+router.get('/:planetName', async (req, res) => {
+  const moonsCollection = req.app.locals.db.collection('moons');
+  const moons = await moonsCollection.find({ planet: req.params.planetName }).toArray();
+  res.json(moons);
+});
+
+router.get('/:planetName/:moonName', async (req, res) => {
+  const moonsCollection = req.app.locals.db.collection('moons');
+  const moon = await moonsCollection.findOne({ name: req.params.moonName });
+  res.json(moon);
 });
 
 module.exports = router;
