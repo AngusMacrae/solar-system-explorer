511b18f291fdf99ff10c5f5d3c1bcd41dd3d1153
Angus Macrae <angusdmacrae@gmail.com>
2020-06-15 12:52:14 +0800

    Move planets, moons and sol display details to separate JSON files

--- client/src/views/PlanetSystem.vue
+++ client/src/views/PlanetSystem.vue
@@ -1,31 +1,27 @@
 <template>
   <div id="planet-system">
-    <Sol :name="planet.name" :fillColour="planet.bulkColour" :diameter="planet.diameter" />
-    <Satellite v-for="(moon, index) in moons" :key="index" :name="moon.name" :fillColour="moon.bulkColour" :diameter="moon.diameter" :orbitDiameter="planet.diameter" :transitionDelay="(index * 2 + 2) / 10" :flexOrder="0" />
+    <Sol :name="sol.name" :fillColour="sol.bulkColour" :diameter="sol.diameter_ss" />
+    <!-- <Sol :name="planet.name" :fillColour="planet.bulkColour" :diameter="planet.diameter" /> -->
+    <Satellite v-for="(planet, index) in planets" :key="index" :name="planet.name" :fillColour="planet.bulkColour" :diameter="planet.diameter_ss" :orbitDiameter="sol.diameter_ss" :transitionDelay="(index * 2 + 2) / 10" :flexOrder="0" />
+    <Satellite v-for="(moon, index) in moons" :key="index" :name="moon.name" :fillColour="moon.bulkColour" :diameter="moon.diameter_ps" :orbitDiameter="planet.diameter" :transitionDelay="(index * 2 + 2) / 10" :flexOrder="index * 2" />
   </div>
 </template>
 
 <script>
 import Sol from '@/components/Sol.vue';
 import Satellite from '@/components/Satellite.vue';
+import sol from '@/data/sol.json';
+import planets from '@/data/planets.json';
+import moons from '@/data/moons.json';
 
 export default {
   name: 'PlanetSystem',
   data() {
     return {
-      planet: {
-        name: this.$route.params.bodyName,
-        bulkColour: '#6b93d6',
-        diameter: 3000,
-      },
-      // moons: fetch('http://localhost:5000/api/moons/' + this.$route.params.bodyName).then(response => response.json()),
-      moons: [
-        {
-          name: 'Moon',
-          bulkColour: '#DBCECA',
-          diameter: 500,
-        },
-      ],
+      sol,
+      planets,
+      planet: planets.filter(planet => planet.name == this.$route.params.bodyName),
+      moons: moons.filter(moon => moon.planet == this.$route.params.bodyName),
     };
   },
   components: {
--- client/src/views/SolarSystem.vue
+++ client/src/views/SolarSystem.vue
@@ -1,65 +1,22 @@
 <template>
   <div id="solar-system">
-    <Sol :name="sol.name" :fillColour="sol.bulkColour" :diameter="sol.diameter" />
-    <Satellite v-for="(planet, index) in planets" :key="index" :name="planet.name" :fillColour="planet.bulkColour" :diameter="planet.diameter" :orbitDiameter="sol.diameter" :transitionDelay="(index * 2 + 2) / 10" :flexOrder="0" />
+    <Sol :name="sol.name" :fillColour="sol.bulkColour" :diameter="sol.diameter_ss" />
+    <Satellite v-for="(planet, index) in planets" :key="index" :name="planet.name" :fillColour="planet.bulkColour" :diameter="planet.diameter_ss" :orbitDiameter="sol.diameter_ss" :transitionDelay="(index * 2 + 2) / 10" :flexOrder="0" />
   </div>
 </template>
 
 <script>
 import Sol from '@/components/Sol.vue';
 import Satellite from '@/components/Satellite.vue';
+import sol from '@/data/sol.json';
+import planets from '@/data/planets.json';
 
 export default {
   name: 'SolarSystem',
   data() {
     return {
-      sol: {
-        name: 'Sol',
-        bulkColour: '#fbd813',
-        diameter: 5000,
-      },
-      planets: [
-        {
-          name: 'Mercury',
-          bulkColour: '#DBCECA',
-          diameter: 25,
-        },
-        {
-          name: 'Venus',
-          bulkColour: '#8B7D82',
-          diameter: 55,
-        },
-        {
-          name: 'Earth',
-          bulkColour: '#6b93d6',
-          diameter: 60,
-        },
-        {
-          name: 'Mars',
-          bulkColour: '#bc2731',
-          diameter: 35,
-        },
-        {
-          name: 'Jupiter',
-          bulkColour: '#d8ca9d',
-          diameter: 250,
-        },
-        {
-          name: 'Saturn',
-          bulkColour: '#ab604a',
-          diameter: 200,
-        },
-        {
-          name: 'Uranus',
-          bulkColour: '#bbe1e4',
-          diameter: 100,
-        },
-        {
-          name: 'Neptune',
-          bulkColour: '#73ACAC',
-          diameter: 100,
-        },
-      ],
+      sol,
+      planets,
     };
   },
   components: {
