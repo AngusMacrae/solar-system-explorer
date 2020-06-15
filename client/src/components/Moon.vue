60db5ad0e56acd0814678acd15318f6a56186962
Angus Macrae <angusdmacrae@gmail.com>
2020-06-15 13:39:37 +0800

    Add Moon component

---
+++ client/src/components/Moon.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="moon"></div>
+</template>
+
+<script>
+export default {
+  name: 'Moon',
+  props: ['name', 'fillColour', 'diameter'],
+};
+</script>
+
+<style lang="scss" scoped></style>
