55b696846f61c5539537ebc15e52ff61a968e5cc
Angus Macrae <angusdmacrae@gmail.com>
2020-06-16 11:29:49 +0800

    Add label and router link to moons

--- client/src/components/Moon.vue
+++ client/src/components/Moon.vue
@@ -1,5 +1,9 @@
 <template>
-  <div class="moon" :style="cssVars"></div>
+  <router-link :to="path">
+    <div class="moon" :style="cssVars">
+      <h2>{{name}}</h2>
+    </div>
+  </router-link>
 </template>
 
 <script>
@@ -7,6 +11,9 @@ export default {
   name: 'Moon',
   props: ['name', 'fillColour', 'diameter'],
   computed: {
+    path() {
+      return "/moon/" + this.name;
+    },
     cssVars() {
       return {
         '--diameter': this.diameter / 15 + 'vw',
@@ -18,23 +25,42 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import "../scss/variables";
+
+.moon {
+  border-radius: 50%;
+  background-color: var(--fill-colour);
+  position: relative;
+  transition: all 0.5s ease-in-out;
+
+  &:hover {
+    transform: scale(1.02);
+  }
+
+  h2 {
+    position: absolute;
+    font-size: 1.3rem;
+    transform: translateX(-50%);
+    left: 50%;
+    bottom: -40px;
+    color: $muted-colour;
+  }
+
+  &:hover h2 {
+    color: $primary-colour;
+  }
+}
+
 .planet-system .moon {
   width: 0;
   height: 0;
   opacity: 0;
   margin: 0;
-  transition: all 0.2s;
 }
 
 .planet-system.expanded .moon {
   width: var(--diameter);
   height: var(--diameter);
   opacity: 1;
-  margin: 2vw;
-}
-
-.moon {
-  border-radius: 50%;
-  background-color: var(--fill-colour);
 }
 </style>
