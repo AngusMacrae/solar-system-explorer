883a0235d0db632558b276aaa0f016e1117e112c
Angus Macrae <angusdmacrae@gmail.com>
2020-06-17 11:02:30 +0800

    Disable animations

--- client/src/components/Planet.vue
+++ client/src/components/Planet.vue
@@ -119,8 +119,8 @@ export default {
   transform: rotate(360deg) translateX(var(--orbit-radius)) rotate(-360deg);
   transition: transform 0.5s ease-in-out, width 0.5s ease-in-out,
     height 0.5s ease-in-out, opacity 0.5s ease-in-out;
-  animation: orbit-in-left 1.5s cubic-bezier(0.075, 0.82, 0.165, 1)
-    calc(var(--transition-delay) + 0.5s) 1 normal backwards;
+  // animation: orbit-in-left 1.5s cubic-bezier(0.075, 0.82, 0.165, 1)
+  //   calc(var(--transition-delay) + 0.5s) 1 normal backwards;
 
   &:hover {
     transform: rotate(360deg) translateX(var(--orbit-radius)) rotate(-360deg)
@@ -133,8 +133,8 @@ export default {
     position: relative;
     top: 40px;
     background-color: $bg-colour;
-    animation: fade-in 1s ease-in-out calc(var(--transition-delay) + 1.5s) 1
-      normal backwards;
+    // animation: fade-in 1s ease-in-out calc(var(--transition-delay) + 1.5s) 1
+    //   normal backwards;
   }
 }
 
@@ -154,8 +154,8 @@ export default {
   top: calc(var(--diameter-vw) / 2 - var(--orbit-diameter) / 2);
   right: calc(var(--diameter-vw) / 2);
   transition: opacity 0.3s ease-in-out 0.2s;
-  animation: fade-in 1s ease-out calc(var(--transition-delay) + 1s) 1 normal
-    backwards;
+  // animation: fade-in 1s ease-out calc(var(--transition-delay) + 1s) 1 normal
+  //   backwards;
 }
 
 a {
