aa2cc7913d4d6c4ecf72dca3e02d973400779f7d
Angus Macrae <angusdmacrae@gmail.com>
2020-06-20 16:03:30 +0800

    Improve moon transition and make unclickable when not expanded

--- client/src/components/Moon.vue
+++ client/src/components/Moon.vue
@@ -34,6 +34,11 @@ export default {
   background-image: var(--svg-url);
   position: relative;
   transition: all 0.5s ease-in-out;
+  width: 0;
+  height: 0;
+  opacity: 0;
+  margin: 0;
+  visibility: hidden;
 
   &:hover {
     transform: scale(1.02);
@@ -46,6 +51,8 @@ export default {
     left: 50%;
     bottom: -40px;
     color: $muted-colour;
+    opacity: 0;
+    transition: transform 0.3s ease-in-out, opacity 0.3s;
   }
 
   &:hover h2 {
@@ -53,16 +60,16 @@ export default {
   }
 }
 
-.planet-system .moon {
-  width: 0;
-  height: 0;
-  opacity: 0;
-  margin: 0;
-}
-
-.planet-system.expanded .moon {
-  width: var(--diameter);
-  height: var(--diameter);
-  opacity: 1;
+.expanded {
+  .moon {
+    width: var(--diameter);
+    height: var(--diameter);
+    opacity: 1;
+    visibility: visible;
+  }
+  h2 {
+    transition: opacity 0.3s 0.2s;
+    opacity: 1;
+  }
 }
 </style>
